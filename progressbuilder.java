e080d8c307171ce9c780aff900c610ead7bc55a5
Abhinandan <79076863+Abhinandan080@users.noreply.github.com>
2021-11-29 09:55:38 +0530

    Update progressbuilder.java

--- progressbuilder.java
+++ progressbuilder.java
@@ -1,6 +1,10 @@
-This is just an extra file tho. 
-  This ends with .java because I want to get more java streaks. 
-  I already have so much on the html. 
+//This is just an extra file tho. 
+//  This ends with .java because I want to get more java streaks. 
+ // I already have so much on the html. 
+   
+//  It took me mor than an hour to prublish this in the proper way. 
   
-  btw, The entire code in in the MainActivity.java . 
-  It took me mor than an hour to prublish this in the proper way. 
+//  The code for the progress bar is in: exportToHTML/com/abhinandan/telegramsketchub3progressbarbyabhinandan/MainActivity.java.html . 
+//  MainActivity.java.html is just a copy of MainActivity.java , just to ** share the code ** . MainActity.java.html must be deleted before compiling. 
+    
+ //    . 
